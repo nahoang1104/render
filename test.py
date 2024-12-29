1af940312b546e8314e6f87d9f9d0e5771cb378d
nahoang1104 <nahoang22@clc.fitus.edu.vn>
2024-12-29 21:15:10 +0700

    resize to 50%

--- test.py
+++ test.py
@@ -17,8 +17,8 @@ def process_image(image_path):
         raise ValueError(f"Image not found or unable to load: {image_path}")
     
     height, width = image.shape[:2]  # Get the current dimensions of the image
-    new_width = int(width * 75 / 100)  # Calculate new width
-    new_height = int(height * 75 / 100)  # Calculate new height
+    new_width = int(width * 50 / 100)  # Calculate new width
+    new_height = int(height * 50 / 100)  # Calculate new height
     # Resize the image
     image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
 
